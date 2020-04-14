85f94f2bce9e68b2a1043ff247e1ab569bd11938
jakariaistauk <jakariamd35@gmail.com>
2020-04-14 19:03:07 +0600

    updating useEffect t-94

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -3,8 +3,12 @@ import classes from './Cockpit.css';
 
 const cockpit  = (props) => {
   useEffect( () => {
-    console.log('[Cockpit.js] useEffect');    
-  });
+    console.log('[Cockpit.js] useEffect');
+
+    setTimeout(() => {
+      alert( 'Saved Data to cloud!' );
+    }, 1000);
+  }, []);
   const assignedClasses = [];
   let btnClass = '';
 
